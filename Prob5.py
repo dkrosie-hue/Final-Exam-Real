86700600151d67c286d7490611b58f752c5345cb
dkrosie-hue <dkrosie@willamette.edu>
2026-05-08 11:44:44 -0700

    Add files via upload

--- Prob5.py
+++ Prob5.py
@@ -24,15 +24,31 @@ class Student:
     def __str__(self):
         return f"{self._name}: {self._grade}"
 
-student1 = Student("Dylan", 95)
-#Output: Dylan: 95
-print(student1) #automatically calls the __str__ method.
+def highest_student(students):
 
-        
+    best_student = students[0]
+
+    for student in students:
+        if student.get_grade() > best_student.get_grade():
+            best_student = student
+
+    return best_student
+
+def main():
+
+    s1 = Student("Dylan", 95)
+    s2 = Student("Anna", 88)
+    s3 = Student("Chris", 100)
+
+    students = [s1, s2, s3]
+
+    print(highest_student(students))
+
+if __name__ == "__main__":
+    main()
 
 if __name__ == "__main__":
     figures = LabelGenerator("P ", 0) 
     print(figures.next_label()) 
     print(figures.next_label())
     print(figures.next_label())
-

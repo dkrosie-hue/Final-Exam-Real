ed2b0daa5dd4a7ca7e93fedf9ddc22d0b3175225
dkrosie-hue <dkrosie@willamette.edu>
2026-05-07 18:55:41 -0700

    Add files via upload

--- Prob5.py
+++ Prob5.py
@@ -9,6 +9,21 @@ class LabelGenerator:
         self._count += 1
         return label
 
+class Student:
+
+    def __init__(self, name, grade):
+        self._name = name
+        self._grade = grade
+
+    def get_grade(self):
+        return self._grade
+
+    def set_grade(self, new_grade):
+        self._grade = new_grade
+
+    def __str__(self):
+        return f"{self._name}: {self._grade}"
+
         
 
 if __name__ == "__main__":
